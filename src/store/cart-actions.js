aecdef473de25f8318c16142a3a0a0307e841024
Jonathan Kromer <jdkromer@gmail.com>
2022-04-13 22:58:24 -0400

    finalizing the fetching logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -14,7 +14,10 @@ export const fetchCartData = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(cartActions.replaceCart({
+                items: cartData.items || [],
+                totalQuantity: cartData.totalQuantity
+            }));
 
         } catch (error) {
             dispatch(uiActions.showNotification({
@@ -41,7 +44,7 @@ export const sendCartData = (cart) => {
             const response = await fetch('https://react-http-94d60-default-rtdb.firebaseio.com/cart.json', 
             {
                method: 'PUT',
-               body: JSON.stringify(cart),
+               body: JSON.stringify({ items: cart.items, totalQuantity: cart.totalQuantity }),
             });
     
     
